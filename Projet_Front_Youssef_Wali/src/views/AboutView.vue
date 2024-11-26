b58cda20dfefe3ef09111a5c4de9e1675d6c56b9
Wali-Youssef <117752075+Wali-Youssef@users.noreply.github.com>
2024-11-26 10:30:10 +0100

    feat: (fin du form)

--- Projet_Front_Youssef_Wali/src/views/AboutView.vue
+++ Projet_Front_Youssef_Wali/src/views/AboutView.vue
@@ -1,15 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+import bouton from '@/components/Button.vue';
+import DefaultLayout from '@/layouts/Default.vue';
+import Form from '@/components/form.vue';
+import Item from '@/components/Item.vue';
+import ListParfums from '@/components/ListParfums.vue';
+</script>
+
+
+
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <Form></Form>
+
+
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
