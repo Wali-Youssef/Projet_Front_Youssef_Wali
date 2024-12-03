baa41e288fa8cf5c00580864fe33184b03407067
Wali-Youssef <117752075+Wali-Youssef@users.noreply.github.com>
2024-12-03 09:16:09 +0100

    feat(sanity): installation  de sanity

---
+++ pages/connexion.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="test">
+    
+    
+    <Form/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+/* Appliquer une image de fond à la classe "test" */
+.test {
+  background-image: url('/assets/img/fond.jpg'); /* Remplacez par le chemin de votre image */
+  background-size: cover; /* L'image couvre toute la zone */
+  background-repeat: no-repeat; /* Pas de répétition de l'image */
+  background-position: center; /* Centrer l'image */
+  min-height: 100vh; /* La hauteur minimale est celle de la fenêtre */
+
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
